0fe6b1bf52dbaa6d35f2f9dbd97a644f2286542a
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2021-12-10 09:28:35 +0100

    fix normals for emissive geometry

--- Assets/Shaders/EmissiveGeom.fs.glsl
+++ Assets/Shaders/EmissiveGeom.fs.glsl
@@ -7,6 +7,6 @@ layout(location=1) out vec4 color1_out;
 
 void main()
 {
-	color0_out = vec4(color_in.rgb, 0);
-	color1_out = vec4(0, 0, 0, 0);
+	color0_out = vec4(0);
+	color1_out = vec4(0.5, 1.0, 1.0, 0);
 }
