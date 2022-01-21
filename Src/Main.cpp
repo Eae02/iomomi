166332af885a847d1852e4ab2c752a2d70e0d17c
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2022-01-21 12:25:14 +0100

    added progress report for asset loading

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -8,6 +8,13 @@
 
 #include <google/protobuf/stubs/common.h>
 
+#ifdef __EMSCRIPTEN__
+#include <sstream>
+#include <iomanip>
+#include <emscripten/emscripten.h>
+#include <emscripten/fetch.h>
+#endif
+
 #ifdef _WIN32
 extern "C"
 {
@@ -25,6 +32,8 @@ bool audioInitializationFailed = false;
 
 void InitializeStaticPropMaterialAsset();
 void InitializeDecalMaterialAsset();
+
+std::vector<char> preloadedAssetBinary;
 
 void Run(int argc, char** argv)
 {
@@ -79,24 +88,101 @@ void Run(int argc, char** argv)
 		eg::console::Init();
 		InitializeStaticPropMaterialAsset();
 		InitializeDecalMaterialAsset();
-		if (!eg::LoadAssets("Assets", "/"))
+		
+		bool assetLoadOK = false;
+		if (!preloadedAssetBinary.empty())
+		{
+			eg::MemoryStreambuf streambuf(preloadedAssetBinary);
+			std::istream eapStream(&streambuf);
+			assetLoadOK = eg::LoadAssetsFromEAPStream(eapStream, "/");
+		}
+		else
+		{
+			assetLoadOK = eg::LoadAssets("Assets", "/");
+		}
+		if (!assetLoadOK)
 		{
 			EG_PANIC("Failed to load assets, make sure assets.eap exists.");
 		}
+		
 		RenderSettings::instance = new RenderSettings;
 		InitLevels();
+		
+#ifdef __EMSCRIPTEN__
+		EM_ASM( loadingComplete(); );
+#endif
 	};
 	
 	eg::Run<Game>(runConfig);
 }
 
 #ifdef __EMSCRIPTEN__
+void AssetDownloadCompleted(emscripten_fetch_t* fetch)
+{
+	EM_ASM( setInfoLabel(""); );
+	
+	preloadedAssetBinary.resize(fetch->numBytes);
+	std::memcpy(preloadedAssetBinary.data(), fetch->data, fetch->numBytes);
+	emscripten_fetch_close(fetch);
+	
+	emscripten_async_call([] (void*)
+	{
+		appDataDirPath = "/data/";
+		char argv[] = "iomomi";
+		char* argvPtr = argv;
+		Run(1, &argvPtr);
+	}, nullptr, 0);
+}
+
+void AssetDownloadFailed(emscripten_fetch_t* fetch)
+{
+	EM_ASM({
+		setInfoLabel('Got response: ' + $0);
+		displayError('Failed to download assets');
+	}, fetch->status);
+	emscripten_fetch_close(fetch);
+}
+
+void AssetDownloadProgress(emscripten_fetch_t* fetch)
+{
+	std::ostringstream messageStream;
+	messageStream << "Downloading assets... (";
+	
+	if (fetch->totalBytes)
+	{
+		messageStream
+			<< std::setprecision(1) << std::fixed << ((double)fetch->dataOffset / 1048576.0) << " / "
+			<< std::setprecision(1) << std::fixed << ((double)fetch->totalBytes / 1048576.0);
+	}
+	else
+	{
+		messageStream << std::setprecision(1) << std::fixed << ((double)(fetch->dataOffset + fetch->numBytes) / 1048576.0);
+	}
+	messageStream << " MiB)";
+	
+	std::string message = messageStream.str();
+	EM_ASM({
+		setInfoLabel(UTF8ToString($0));
+	}, message.c_str());
+}
+
 extern "C" void WebMain()
 {
-	appDataDirPath = "/data/";
-	char argv[] = "iomomi";
-	char* argvPtr = argv;
-	Run(1, &argvPtr);
+	eg::releasePanicCallback = [] (const std::string& message)
+	{
+		EM_ASM({
+			setInfoLabel(UTF8ToString($0));
+		}, message.c_str());
+	};
+	
+	emscripten_fetch_attr_t attr;
+	emscripten_fetch_attr_init(&attr);
+	strcpy(attr.requestMethod, "GET");
+	attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
+	attr.onsuccess = AssetDownloadCompleted;
+	attr.onerror = AssetDownloadFailed;
+	attr.onprogress = AssetDownloadProgress;
+	emscripten_fetch(&attr, "Assets.eap");
 }
 #else
 int main(int argc, char** argv)
